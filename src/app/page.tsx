46e975b3749b2a5da0da55592267a9a0b932cdb2
devclaudiocassimiro <claudio.silva@jala.university>
2024-06-20 10:37:34 -0300

    refactor: add new section and add improvements on contrast of plans sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,9 @@ import {
   FaReact,
   FaNodeJs,
   FaDatabase,
+  FaImagePortrait,
+  FaNetworkWired,
+  FaUsersViewfinder,
 } from "react-icons/fa6";
 import { SiHtmx } from "react-icons/si";
 
@@ -154,6 +157,64 @@ export default function Home() {
           </div>
         </section>
         <hr />
+        <section className="flex flex-col mb-9">
+          {/* A div abaixo vai servir para Mobile */}
+          <div className="flex flex-col lg:hidden">
+            <h2 className="text-2xl md:text-3xl my-5">Carreira</h2>
+            <div className="relative w-72 h-72 self-center md:w-96 md:h-96">
+              <Image
+                src="/images/homem-progredindo-na-carreira.png"
+                alt="pessoas em colaboração"
+                fill
+              />
+            </div>
+            <ul className="flex flex-col gap-2 md:text-2xl">
+              <li className="flex items-center gap-1">
+                <FaNetworkWired size="20" /> Mentoria Profissional
+              </li>
+              <li className="flex items-center gap-1">
+                <FaUsersViewfinder size="20" /> Preparação para Entrevistas
+              </li>
+              <li className="flex items-center gap-1">
+                <FaLinkedin size="20" /> Revisão de Currículos e Perfis LinkedIn
+              </li>
+              <li className="flex items-center gap-1">
+                <FaImagePortrait size="20" /> Apoio na Construção de Portfólio
+              </li>
+            </ul>
+          </div>
+          {/* A div abaixo vai servir para Desktop */}
+          <div className="hidden lg:flex lg:items-center lg:gap-10">
+            <div className="w-[60%]">
+              <h2 className="text-2xl md:text-3xl lg:text-4xl mb-5">
+                Carreira
+              </h2>
+              <ul className="flex flex-col gap-2 lg:text-3xl">
+                <li className="flex items-center gap-1">
+                  <FaNetworkWired size="20" /> Mentoria Profissional
+                </li>
+                <li className="flex items-center gap-1">
+                  <FaUsersViewfinder size="20" /> Preparação para Entrevistas
+                </li>
+                <li className="flex items-center gap-1">
+                  <FaLinkedin size="20" /> Revisão de Currículos e Perfis
+                  LinkedIn
+                </li>
+                <li className="flex items-center gap-1">
+                  <FaImagePortrait size="20" /> Apoio na Construção de Portfólio
+                </li>
+              </ul>
+            </div>
+            <div className="relative self-center w-[650px] h-[600px]">
+              <Image
+                src="/images/homem-progredindo-na-carreira.png"
+                alt="pessoas em colaboração"
+                fill
+              />
+            </div>
+          </div>
+        </section>
+        <hr />
         <section className="flex my-9">
           {/* A div abaixo vai servir para Mobile */}
           <div className="flex flex-col lg:hidden">
@@ -195,6 +256,13 @@ export default function Home() {
           </div>
           {/* A div abaixo vai servir para Desktop */}
           <div className="hidden lg:flex lg:justify-center lg:items-center lg:gap-10">
+            <div className="relative self-center w-[650px] h-[600px]">
+              <Image
+                src="/images/mulher-escolhendo.png"
+                alt="pessoas em fazendo uma escolha"
+                fill
+              />
+            </div>
             <div className="w-[50%]">
               <h2 className="text-2xl md:text-3xl lg:text-4xl mb-5">
                 Por que Escolher a BemCodado?
@@ -225,13 +293,6 @@ export default function Home() {
                 </li>
               </ul>
             </div>
-            <div className="relative self-center w-[650px] h-[600px]">
-              <Image
-                src="/images/mulher-escolhendo.png"
-                alt="pessoas em fazendo uma escolha"
-                fill
-              />
-            </div>
           </div>
         </section>
         <hr />
@@ -245,9 +306,9 @@ export default function Home() {
             BemCodado e transforme suas aspirações em realidade.
           </p>
           <div className="lg:flex lg:flex-wrap lg:gap-4">
-            <section className="flex flex-col justify-between mb-5 border rounded p-2 lg:lg:w-[49%] shadow-xl">
+            <section className="flex flex-col justify-between mb-5 border border-slate-500 rounded p-2 lg:lg:w-[49%] shadow-xl bg-white text-black">
               <div>
-                <h2 className="text-2xl mb-1">Mentoria em Grupo</h2>
+                <h2 className="text-2xl mb-1 font-bold">Mentoria em Grupo</h2>
                 <p className="text-xl mb-1">Aqui você terá:</p>
                 <ul className="flex flex-col gap-3 border rounded p-2 shadow-md mb-2">
                   <li className="text-justify">
@@ -267,13 +328,13 @@ export default function Home() {
               </div>
               <a
                 href="mailto:claudioletras2019@gmail.com?subject=Quero saber mais sobre a Mentoria em Grupo&body=Mentoria%20em%20Grupo%0A%0AAqui%20você%20terá%3A%0A%0A- Sessões%20diárias%20de%20mentoria%20em%20grupo%20no%20Discord%2C%20de%20segunda%20a%20sexta-feira%2C%20com%20foco%20em%20tópicos%20específicos.%0A- Acesso%20a%20materiais%20de%20estudo%2C%20recursos%20e%20exercícios%20práticos.%0A- Suporte%20por%20e-mail%20ou%20mensagem%20para%20dúvidas%20adicionais%20entre%20as%20sessões.%0A%0AValor%20Fixo%20Mensal%3A%20R%24%20250%2C00"
-                className="border block rounded p-3 w-full text-center transition delay-180 hover:bg-sky-500"
+                className="border block rounded p-3 w-full text-center transition delay-180 text-white bg-sky-400 hover:bg-sky-500"
               >
                 Inscrever-se
               </a>
             </section>
-            <section className="mb-5 border rounded p-2 lg:w-[49%] shadow-xl">
-              <h2 className="text-xl mb-1">Mentoria Individual</h2>
+            <section className="mb-5 border border-slate-500 rounded p-2 lg:w-[49%] shadow-xl bg-white text-black">
+              <h2 className="text-xl mb-1 font-bold">Mentoria Individual</h2>
               <p className="text-lg mb-1">Aqui você terá:</p>
               <ul className="flex flex-col gap-3 border rounded p-2 shadow-md mb-2">
                 <li className="text-justify">
@@ -301,13 +362,13 @@ export default function Home() {
               </p>
               <a
                 href="mailto:claudioletras2019@gmail.com?subject=Quero%20saber%20mais%20sobre%20a%20Mentoria%20Individual&body=Mentoria%20Individual%0A%0AAqui%20você%20terá%3A%0A%0A- Sessões%20individuais%20personalizadas%20com%20foco%20nos%20objetivos%20e%20necessidades%20específicas%20de%20cada%20aluno.%0A- Sessões%20disponíveis%20de%20segunda%20a%20sexta-feira.%0A- Acesso%20prioritário%20para%20agendamento%20de%20sessões.%0A- Revisão%20e%20feedback%20detalhados%20sobre%20projetos%2C%20portfólio%20ou%20trabalhos%20específicos.%0A- Suporte%20por%20e-mail%20ou%20mensagem%20para%20dúvidas%20adicionais%20entre%20as%20sessões.%0A%0AValor%20Fixo%20Mensal%3A%20R%24%20500%2C00%0AValor%20Avulso%20por%20Sessão%20Individual%3A%20R%24%2075%2C00"
-                className="border block rounded p-3 w-full text-center transition delay-180 hover:bg-sky-500"
+                className="border block rounded p-3 w-full text-center transition delay-180 text-white bg-sky-400 hover:bg-sky-500"
               >
                 Inscrever-se
               </a>
             </section>
-            <section className="mb-5 border rounded p-2 lg:w-[49%] shadow-xl">
-              <h2 className="text-xl mb-1">Plano de 6 Meses</h2>
+            <section className="mb-5 border border-slate-500 rounded p-2 lg:w-[49%] shadow-xl bg-white text-black">
+              <h2 className="text-xl mb-1 font-bold">Plano de 6 Meses</h2>
               <p className="text-lg mb-1">Mentoria em Grupo:</p>
               <ul className="flex flex-col gap-3 border rounded p-2 shadow-md mb-2">
                 <li className="text-justify">
@@ -330,13 +391,13 @@ export default function Home() {
               </ul>
               <a
                 href="mailto:claudioletras2019@gmail.com?subject=Quero%20assinar%20o%20plano%20de%206%20meses&body=Plano%20de%206%20Meses%0A%0AMentoria%20em%20Grupo%3A%0A%0A- Valor Mensal%3A%20de%20R%24%20250%2C00%20por%20R%24%20210%2C00%0A- Valor Total%20%286%20Meses%29%3A%20R%24%201.260%2C00%20%28desconto%20de%20R%24%20240%2C00%29%0A%0AMentoria%20Individual%3A%0A%0A- Valor Mensal%3A%20de%20R%24%20504%2C00%20por%20R%24%20416%2C00%0A- Valor Total%20%286%20Meses%29%3A%20R%24%202.496%2C00%20%28desconto%20de%20R%24%20504%2C00%29"
-                className="border block rounded p-3 w-full text-center transition delay-180 hover:bg-sky-500"
+                className="border block rounded p-3 w-full text-center transition delay-180 text-white bg-sky-400 hover:bg-sky-500"
               >
                 Inscrever-se
               </a>
             </section>
-            <section className="mb-5 border rounded p-2 lg:w-[49%] shadow-xl">
-              <h2 className="text-xl mb-1">Plano de 1 Ano</h2>
+            <section className="mb-5 border border-slate-500 rounded p-2 lg:w-[49%] shadow-xl bg-white text-black">
+              <h2 className="text-xl mb-1 font-bold">Plano de 1 Ano</h2>
               <p className="text-lg mb-1">Mentoria em Grupo:</p>
               <ul className="flex flex-col gap-3 border rounded p-2 shadow-md mb-2">
                 <li className="text-justify">
@@ -359,7 +420,7 @@ export default function Home() {
               </ul>
               <a
                 href="mailto:claudioletras2019@gmail.com?subject=Quero%20assinar%20o%20plano%20de%201%20ano&body=Plano%20de%201%20Ano%0A%0AMentoria%20em%20Grupo%3A%0A%0A- Valor Mensal%3A%20de%20R%24%20250%2C00%20por%20R%24%20208%2C00%0A- Valor Total%20%281%20Ano%29%3A%20R%24%202.496%2C00%20%28desconto%20de%20R%24%20504%2C00%29%0A%0AMentoria%20Individual%3A%0A%0A- Valor Mensal%3A%20de%20R%24%20500%2C00%20por%20R%24%20416%2C00%0A- Valor Total%20%281%20Ano%29%3A%20R%24%204.992%2C00%20%28desconto%20de%20R%24%201.008%2C00%29"
-                className="border block rounded p-3 w-full text-center transition delay-180 hover:bg-sky-500"
+                className="border block rounded p-3 w-full text-center transition delay-180 text-white bg-sky-400 hover:bg-sky-500"
               >
                 Inscrever-se
               </a>
